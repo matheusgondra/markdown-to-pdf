731ff8ae36ffba24d3f99dfcf4d5fb92fd43675f
matheusgondra <matheusgondra951@gmail.com>
2026-03-23 23:24:51 -0300

    feat: create ConvertMarkdownToPdfController

---
+++ src/main/java/com/matheusgondra/md2pdf/controller/ConvertMarkdownToPdfController.java
@@ -0,0 +1,36 @@
+package com.matheusgondra.md2pdf.controller;
+
+import com.matheusgondra.md2pdf.service.PdfService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/markdown-to-pdf")
+public class ConvertMarkdownToPdfController {
+    private final PdfService service;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<byte[]> handle(@RequestParam("file") MultipartFile file) {
+        try {
+            byte[] pdfBytes = service.execute(file);
+            String outputName = Objects.requireNonNull(file.getOriginalFilename()).replace(".md", ".pdf");
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDisposition(ContentDisposition.attachment().filename(outputName).build());
+
+            return ResponseEntity.ok().headers(headers).body(pdfBytes);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+}
