d4a749a192d2ac3ab42e6bdb269fc8b85abf1114
matheusgondra <matheusgondra951@gmail.com>
2026-03-23 21:52:31 -0300

    commit initial

---
+++ src/main/java/com/matheusgondra/md2pdf/MD2PDFApplication.java
@@ -0,0 +1,13 @@
+package com.matheusgondra.md2pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MD2PDFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MD2PDFApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusgondra/md2pdf/Md2pdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusgondra.md2pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md2pdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
