cdfc256eb4e1ff5b22a2fac66fdd4b499bd447bd
matheusgondra <matheusgondra951@gmail.com>
2026-03-27 08:37:54 -0300

    docs: update docs

--- src/main/java/com/matheusgondra/md2pdf/controller/doc/ConvertMarkdownToPdfControllerDoc.java
+++ src/main/java/com/matheusgondra/md2pdf/controller/doc/ConvertMarkdownToPdfControllerDoc.java
@@ -5,6 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.matheusgondra.md2pdf.exception.ErrorResponse;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -14,13 +16,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @Operation(summary = "Convert Markdown to PDF", description = "Converts an uploaded Markdown file to PDF format and returns it as PDF file download.")
 @ApiResponses({
-                @ApiResponse(responseCode = "200", description = "PDF file generated successfully", headers = {
-                                @Header(name = "Content-Type", description = "application/pdf", schema = @Schema(type = "string")),
-                                @Header(name = "Content-Disposition", description = "attachment; filename=arquivo.pdf", schema = @Schema(type = "string"))
-                }, content = @Content(mediaType = "application/pdf", schema = @Schema(type = "string", format = "binary"))),
-                @ApiResponse(responseCode = "400", description = "Invalid file format or missing file"),
-                @ApiResponse(responseCode = "413", description = "Uploaded file exceeds the maximum allowed size"),
-                @ApiResponse(responseCode = "500", description = "Internal server error occurred during conversion")
+        @ApiResponse(responseCode = "200", description = "PDF file generated successfully", headers = {
+                @Header(name = "Content-Type", description = "application/pdf", schema = @Schema(type = "string")),
+                @Header(name = "Content-Disposition", description = "attachment; filename=arquivo.pdf", schema = @Schema(type = "string"))
+        }, content = @Content(mediaType = "application/pdf", schema = @Schema(type = "string", format = "binary"))),
+        @ApiResponse(responseCode = "400", description = "Invalid file format or missing file", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(responseCode = "413", description = "Uploaded file exceeds the maximum allowed size"),
+        @ApiResponse(responseCode = "500", description = "Internal server error occurred during conversion", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
 })
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
